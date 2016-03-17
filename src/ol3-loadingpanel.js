885a26d1204a2ec8837c9c47043081b48e0c72da
eblondel <emmanuel.blondel1@gmail.com>
2016-03-17 15:17:46 +0100

    #3 wrap ol3-loadingpanel.js in AMD and CJS format structure

--- src/ol3-loadingpanel.js
+++ src/ol3-loadingpanel.js
@@ -1,5 +1,6 @@
 /**
- * Copyright (c) 2015 Emmanuel Blondel
+ * ol3-loadingpanel - v1.0.1 - 2016-03-17
+ * Copyright (c) 2016 Emmanuel Blondel
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
  * and associated documentation files (the "Software"), to deal in the Software without restriction, 
@@ -30,260 +31,278 @@
  * @author Emmanuel Blondel
  *
  */
-ol.control.LoadingPanel = function(opt_options) {
-	
-	var options = opt_options || {};
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define([], function () {
+      return (factory());
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    factory();
+  }
+}(this, function () {
+ 
+	  ol.control.LoadingPanel = function(opt_options) {
+		
+		var options = opt_options || {};
 
-    	this.mapListeners = [];
+			this.mapListeners = [];
 
-	this.tileListeners = [];
+		this.tileListeners = [];
 
-	this.loadStatus_ = false;
+		this.loadStatus_ = false;
 
-	this.loadProgress_ = [0,1];
-	
-	//widget type
-	if(options.widget) if(['animatedgif', 'progressbar'].indexOf(options.widget) == -1) alert("invalid value for 'widget'");
-	this.widget = (options.widget)? options.widget : 'animatedgif';
+		this.loadProgress_ = [0,1];
+		
+		//widget type
+		if(options.widget) if(['animatedgif', 'progressbar'].indexOf(options.widget) == -1) alert("invalid value for 'widget'");
+		this.widget = (options.widget)? options.widget : 'animatedgif';
 
-	//progress mode
-	if(options.progressMode) if(['tile','layer'].indexOf(options.progressMode) == -1) alert("invalid value for 'progressMode'");
-	this.loadProgressByTile_ = ( options.progressMode == 'layer')? false : true;
-	
-	//other options
-	this.showPanel = (typeof options.showPanel == 'boolean') ? options.showPanel : true;
-	
-	//class name
-	var className = options.className ? options.className : 'ol-loading-panel';
-	
-	//element
-	var elementDom = (this.widget == 'animatedgif')? 'span' : 'progress';
-	var element = document.createElement(elementDom);
-	element.className = className + ' ' + 'ol-unselectable';
-	if(this.widget == 'progressbar') {
-		//element progress bar for old browsers
-		var div = document.createElement('div');
-		div.className = 'ol-progress-bar';
-		var span = document.createElement('span');
-		div.appendChild(span);
-	}
-
-	//events
-	this.oncustomstart = (options.onstart)? options.onstart : false;
-	this.oncustomprogress = (options.onprogress)? options.onprogress : false;
-	this.oncustomend = (options.onend)? options.onend : false;
-
-	ol.control.Control.call(this, {
-		element: element,
-		target: options.target
-	});
-};
-
-ol.inherits(ol.control.LoadingPanel, ol.control.Control);
-
-/**
- * Setup the loading panel
- */
-ol.control.LoadingPanel.prototype.setup = function() {
-	var size = this.getMap().getSize();
-	this.element.style.left = String( Math.round( size[0]/2 ) ) + 'px';
-    this.element.style.bottom = String( Math.round( size[1]/2 ) ) + 'px';
-	
-	var this_ = this;
-
-	this.mapListeners.push(this.getMap().on('pointerdown', function() {
-		this_.hide();
-	}));
-
-	//display loading panel before render
-	this.mapListeners.push(this.getMap().beforeRender(function(map,framestate){
-		this_.registerLayersLoadEvents_();
-		this_.show();
-		if(this_.oncustomstart) this_.oncustomstart.apply(this_,[]);
-	}));
-
-	//hide loading panel after render
-	this.mapListeners.push(this.getMap().on("postrender", function(e){
-		this_.updateLoadStatus_();
-		if( this_.loadStatus_ ){
-			if(this_.oncustomend) this_.oncustomend.apply(this_,[]);
-			this_.hide();
+		//progress mode
+		if(options.progressMode) if(['tile','layer'].indexOf(options.progressMode) == -1) alert("invalid value for 'progressMode'");
+		this.loadProgressByTile_ = ( options.progressMode == 'layer')? false : true;
+		
+		//other options
+		this.showPanel = (typeof options.showPanel == 'boolean') ? options.showPanel : true;
+		
+		//class name
+		var className = options.className ? options.className : 'ol-loading-panel';
+		
+		//element
+		var elementDom = (this.widget == 'animatedgif')? 'span' : 'progress';
+		var element = document.createElement(elementDom);
+		element.className = className + ' ' + 'ol-unselectable';
+		if(this.widget == 'progressbar') {
+			//element progress bar for old browsers
+			var div = document.createElement('div');
+			div.className = 'ol-progress-bar';
+			var span = document.createElement('span');
+			div.appendChild(span);
 		}
-	}));
-	
-};
 
+		//events
+		this.oncustomstart = (options.onstart)? options.onstart : false;
+		this.oncustomprogress = (options.onprogress)? options.onprogress : false;
+		this.oncustomend = (options.onend)? options.onend : false;
 
-/**
- * Reports load progress for a source
- * @param source
- * @return true if complete false otherwise
- */
-ol.control.LoadingPanel.prototype.updateSourceLoadStatus_ = function(source){
-	return Math.round(source.loaded / source.loading * 100) == 100;
-}
+		ol.control.Control.call(this, {
+			element: element,
+			target: options.target
+		});
+	  };
 
-/**
- * Register layer load events
- * @param layer
- */
-ol.control.LoadingPanel.prototype.registerLayerLoadEvents_ = function(layer) {
-	
-	var this_ = this;
+	  ol.inherits(ol.control.LoadingPanel, ol.control.Control);
 
-	layer.getSource().on("tileloadstart", function(e) {
-		if( this_.loadStatus_ ) {
-			this_.loadStatus_ = false;
-			this_.loadProgress_ = [0,1];
-			if(this_.widget == 'progressbar') {
-				this_.element.value = this_.loadProgress_[0];
-				this_.element.max = this_.loadProgress_[1];
-			}
+	/**
+	 * Setup the loading panel
+	 */
+	ol.control.LoadingPanel.prototype.setup = function() {
+		var size = this.getMap().getSize();
+		this.element.style.left = String( Math.round( size[0]/2 ) ) + 'px';
+		this.element.style.bottom = String( Math.round( size[1]/2 ) ) + 'px';
+		
+		var this_ = this;
+
+		this.mapListeners.push(this.getMap().on('pointerdown', function() {
+			this_.hide();
+		}));
+
+		//display loading panel before render
+		this.mapListeners.push(this.getMap().beforeRender(function(map,framestate){
+			this_.registerLayersLoadEvents_();
 			this_.show();
 			if(this_.oncustomstart) this_.oncustomstart.apply(this_,[]);
-		}
-		this.loading = (this.loading)? this.loading+1 : 1;
-		this.isLoaded = this_.updateSourceLoadStatus_(this);
-		if( this_.loadProgressByTile_) {
-			this_.loadProgress_[1] += 1;
-			if(this_.widget == 'progressbar'){
-				this_.element.max = this_.loadProgress_[1];
-				var progressBarDiv = this_.element.getElementsByClassName('ol-progress-bar');
-				if( progressBarDiv.length > 0 ) progressBarDiv[0].children()[0].width = String(parseInt(100 * this_.progress()))+'%';
-			}
-		}
-	});
-	layer.getSource().on("tileloadend", function(e) {
-		this.loaded = (this.loaded)? this.loaded+1 : 1;
-		this.isLoaded = this_.updateSourceLoadStatus_(this);
-		if( this_.loadProgressByTile_) {
-			this_.loadProgress_[0] += 1;
-			if(this_.widget == 'progressbar'){
-				this_.element.value = this_.loadProgress_[0];
-				var progressBarDiv = this_.element.getElementsByClassName('ol-progress-bar');
-				if( progressBarDiv.length > 0 ) progressBarDiv[0].children()[0].width = String(parseInt(100 * this_.progress()))+'%';
-			}
-			if(this_.oncustomprogress) this_.oncustomprogress.apply(this_,this_.loadProgress_);
-		}
+		}));
 
-	});
-}
-
-/**
- * Register layer load events
- *
- */
-ol.control.LoadingPanel.prototype.registerLayersLoadEvents_ = function() {
-	var groups = this.getMap().getLayers().getArray();
-	for(var i=0;i<groups.length;i++) {
-		var layer = groups[i];
-		if(layer instanceof ol.layer.Group) {
-			var layers = layer.getLayers().getArray();
-			for(var j=0;j<layers.length;j++){
-				var l = layers[j];
-				if( !(l instanceof ol.layer.Vector) ) {
-					this.tileListeners.push( this.registerLayerLoadEvents_(l) );
-				}
+		//hide loading panel after render
+		this.mapListeners.push(this.getMap().on("postrender", function(e){
+			this_.updateLoadStatus_();
+			if( this_.loadStatus_ ){
+				if(this_.oncustomend) this_.oncustomend.apply(this_,[]);
+				this_.hide();
 			}
-		} else if (layer instanceof ol.layer.Layer) {
-			if( !(layer instanceof ol.layer.Vector) ) {
-				this.tileListeners.push( this.registerLayerLoadEvents_(layer) );
-			}
-		}
-	}
-}
-
-/**
- * Gives a load status for the complete stack of layers
- *
- */
-ol.control.LoadingPanel.prototype.updateLoadStatus_ = function() {
-	
-
-	var loadStatusArray = new Array();
-	var groups = this.getMap().getLayers().getArray();
-	for(var i=0;i<groups.length;i++) {
-		var layer = groups[i];
-		if(layer instanceof ol.layer.Group) {
-			var layers = layer.getLayers().getArray();
-			for(var j=0;j<layers.length;j++){
-				var l = layers[j];
-				if( !(l instanceof ol.layer.Vector) ) {
-					loadStatusArray.push( l.getSource().isLoaded );
-				}
-			}
-		} else {
-			loadStatusArray.push( layer.getSource().isLoaded );	
-		}
-	}
+		}));
 		
-	//status
-	this.loadStatus_ = (loadStatusArray.indexOf(false) == - 1) && (loadStatusArray.indexOf(true) != -1);
+	};
 
 
-	if( !this.loadProgressByTile_ ) {
+	/**
+	 * Reports load progress for a source
+	 * @param source
+	 * @return true if complete false otherwise
+	 */
+	ol.control.LoadingPanel.prototype.updateSourceLoadStatus_ = function(source){
+		return Math.round(source.loaded / source.loading * 100) == 100;
+	}
 
-		//progress
-		var count = {}
-		loadStatusArray.forEach(function(i) { count[i] = (count[i]||0)+1;  });
-		var loaded = (count[true])? count[true] : 0;
-	
-		//progress events
-		if( loaded > this.loadProgress_[0]){
-			this.loadProgress_ = [loaded,loadStatusArray.length];
-			if(this_.widget == 'progressbar') {
-				this_.element.max = this_.loadProgress_[1];
-				this_.element.value = this_.loadProgress_[0];
+	/**
+	 * Register layer load events
+	 * @param layer
+	 */
+	ol.control.LoadingPanel.prototype.registerLayerLoadEvents_ = function(layer) {
+		
+		var this_ = this;
+
+		layer.getSource().on("tileloadstart", function(e) {
+			if( this_.loadStatus_ ) {
+				this_.loadStatus_ = false;
+				this_.loadProgress_ = [0,1];
+				if(this_.widget == 'progressbar') {
+					this_.element.value = this_.loadProgress_[0];
+					this_.element.max = this_.loadProgress_[1];
+				}
+				this_.show();
+				if(this_.oncustomstart) this_.oncustomstart.apply(this_,[]);
 			}
-			if(this.oncustomprogress) this.oncustomprogress.apply(this,this.loadProgress_);
+			this.loading = (this.loading)? this.loading+1 : 1;
+			this.isLoaded = this_.updateSourceLoadStatus_(this);
+			if( this_.loadProgressByTile_) {
+				this_.loadProgress_[1] += 1;
+				if(this_.widget == 'progressbar'){
+					this_.element.max = this_.loadProgress_[1];
+					var progressBarDiv = this_.element.getElementsByClassName('ol-progress-bar');
+					if( progressBarDiv.length > 0 ) progressBarDiv[0].children()[0].width = String(parseInt(100 * this_.progress()))+'%';
+				}
+			}
+		});
+		layer.getSource().on("tileloadend", function(e) {
+			this.loaded = (this.loaded)? this.loaded+1 : 1;
+			this.isLoaded = this_.updateSourceLoadStatus_(this);
+			if( this_.loadProgressByTile_) {
+				this_.loadProgress_[0] += 1;
+				if(this_.widget == 'progressbar'){
+					this_.element.value = this_.loadProgress_[0];
+					var progressBarDiv = this_.element.getElementsByClassName('ol-progress-bar');
+					if( progressBarDiv.length > 0 ) progressBarDiv[0].children()[0].width = String(parseInt(100 * this_.progress()))+'%';
+				}
+				if(this_.oncustomprogress) this_.oncustomprogress.apply(this_,this_.loadProgress_);
+			}
+
+		});
+	}
+
+	/**
+	 * Register layer load events
+	 *
+	 */
+	ol.control.LoadingPanel.prototype.registerLayersLoadEvents_ = function() {
+		var groups = this.getMap().getLayers().getArray();
+		for(var i=0;i<groups.length;i++) {
+			var layer = groups[i];
+			if(layer instanceof ol.layer.Group) {
+				var layers = layer.getLayers().getArray();
+				for(var j=0;j<layers.length;j++){
+					var l = layers[j];
+					if( !(l instanceof ol.layer.Vector) ) {
+						this.tileListeners.push( this.registerLayerLoadEvents_(l) );
+					}
+				}
+			} else if (layer instanceof ol.layer.Layer) {
+				if( !(layer instanceof ol.layer.Vector) ) {
+					this.tileListeners.push( this.registerLayerLoadEvents_(layer) );
+				}
+			}
 		}
 	}
 
-}
+	/**
+	 * Gives a load status for the complete stack of layers
+	 *
+	 */
+	ol.control.LoadingPanel.prototype.updateLoadStatus_ = function() {
+		
 
-/**
- * Show the loading panel
- */
-ol.control.LoadingPanel.prototype.show = function() {
-	if ( this.showPanel ) this.element.style.display = 'block';
-};
-
-/**
- * Hide the loading panel
- */
-ol.control.LoadingPanel.prototype.hide = function() {
-	if ( this.showPanel ) this.element.style.display = 'none';
-};
-
-/**
- * Show the progress details
- */
-ol.control.LoadingPanel.prototype.progressDetails = function() {
-	return this.loadProgress_;
-};
-
-/**
- * Show the progress details
- */
-ol.control.LoadingPanel.prototype.progress = function() {
-	return this.loadProgress_[0] / this.loadProgress_[1];	
-};
+		var loadStatusArray = new Array();
+		var groups = this.getMap().getLayers().getArray();
+		for(var i=0;i<groups.length;i++) {
+			var layer = groups[i];
+			if(layer instanceof ol.layer.Group) {
+				var layers = layer.getLayers().getArray();
+				for(var j=0;j<layers.length;j++){
+					var l = layers[j];
+					if( !(l instanceof ol.layer.Vector) ) {
+						loadStatusArray.push( l.getSource().isLoaded );
+					}
+				}
+			} else {
+				loadStatusArray.push( layer.getSource().isLoaded );	
+			}
+		}
+			
+		//status
+		this.loadStatus_ = (loadStatusArray.indexOf(false) == - 1) && (loadStatusArray.indexOf(true) != -1);
 
 
-/**
- * Set the map instance the control is associated with.
- * @param {ol.Map} map The map instance.
- */
-ol.control.LoadingPanel.prototype.setMap = function(map) {
-    
-	// Clean up listeners associated with the previous map
-    	for (var i = 0, key; i < this.mapListeners.length; i++) {
-        	this.getMap().unByKey(this.mapListeners[i]);
-    	}
-	
-    	this.mapListeners.length = 0;
-        
-	ol.control.Control.prototype.setMap.call(this, map);
-    	if (map) this.setup();
+		if( !this.loadProgressByTile_ ) {
 
-};
+			//progress
+			var count = {}
+			loadStatusArray.forEach(function(i) { count[i] = (count[i]||0)+1;  });
+			var loaded = (count[true])? count[true] : 0;
+		
+			//progress events
+			if( loaded > this.loadProgress_[0]){
+				this.loadProgress_ = [loaded,loadStatusArray.length];
+				if(this_.widget == 'progressbar') {
+					this_.element.max = this_.loadProgress_[1];
+					this_.element.value = this_.loadProgress_[0];
+				}
+				if(this.oncustomprogress) this.oncustomprogress.apply(this,this.loadProgress_);
+			}
+		}
+
+	}
+
+	/**
+	 * Show the loading panel
+	 */
+	ol.control.LoadingPanel.prototype.show = function() {
+		if ( this.showPanel ) this.element.style.display = 'block';
+	};
+
+	/**
+	 * Hide the loading panel
+	 */
+	ol.control.LoadingPanel.prototype.hide = function() {
+		if ( this.showPanel ) this.element.style.display = 'none';
+	};
+
+	/**
+	 * Show the progress details
+	 */
+	ol.control.LoadingPanel.prototype.progressDetails = function() {
+		return this.loadProgress_;
+	};
+
+	/**
+	 * Show the progress details
+	 */
+	ol.control.LoadingPanel.prototype.progress = function() {
+		return this.loadProgress_[0] / this.loadProgress_[1];	
+	};
+
+
+	/**
+	 * Set the map instance the control is associated with.
+	 * @param {ol.Map} map The map instance.
+	 */
+	ol.control.LoadingPanel.prototype.setMap = function(map) {
+		
+		// Clean up listeners associated with the previous map
+			for (var i = 0, key; i < this.mapListeners.length; i++) {
+				this.getMap().unByKey(this.mapListeners[i]);
+			}
+		
+			this.mapListeners.length = 0;
+			
+		ol.control.Control.prototype.setMap.call(this, map);
+			if (map) this.setup();
+
+	};
+
+}));
