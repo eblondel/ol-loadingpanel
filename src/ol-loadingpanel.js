470ade36cda5d476228121cedb420fed1d61e1e8
eblondel <emmanuel.blondel1@gmail.com>
2020-11-20 19:58:28 +0100

    #12

--- src/ol-loadingpanel.js
+++ src/ol-loadingpanel.js
@@ -1,10 +1,28 @@
+/**
+ * ol-loadingpanel - v2.0.0 - 2020-11-20
+ * Copyright (c) 2016 Emmanuel Blondel
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 import Control from 'ol/control/Control';
 import {Group as LayerGroup, Tile as TileLayer, Image as ImageLayer, Vector as VectorLayer} from 'ol/layer';
 import Layer from 'ol/layer/Layer';
 import {TileImage} from 'ol/source';
 import { unByKey } from 'ol/Observable';
-import './ol-loadingpanel.css';
 
 /**
  * OpenLayers Loading Panel Control.
@@ -190,6 +208,7 @@ export default class LoadingPanel extends Control {
 				for(var j=0;j<layers.length;j++){
 					var l = layers[j];
 					if( !(l instanceof VectorLayer) ) {
+						console.log(l);
 						this.tileListeners.push( this.registerLayerLoadEvents_(l) );
                         this.layerCount += 1;
 					}
